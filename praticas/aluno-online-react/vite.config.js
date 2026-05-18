18fd82b938d092e5705024f2c13133b1f0406811
MsAnaCode <ana.sousa30@iesb.edu.br>
2026-05-18 10:21:03 -0300

    resolvendo outros conflitos da pratica05

--- praticas/aluno-online-react/vite.config.js
+++ praticas/aluno-online-react/vite.config.js
@@ -1,19 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
-import tailwindcss from '@tailwindcss/vite' // <-- 1. Importou aqui
-=======
 import tailwindcss from '@tailwindcss/vite' // <-- Importe o plugin aqui
->>>>>>> 281a770e1821c02ed5fa01d0be7056ce8394c78c
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-<<<<<<< HEAD
-    tailwindcss(), // <-- 2. Adicionou aqui
-  ],
-=======
     tailwindcss()
   ],
   // Configuração do Servidor para Docker  
@@ -27,5 +19,4 @@ export default defineConfig({
       clientPort: 5173,
     },
   },
->>>>>>> 281a770e1821c02ed5fa01d0be7056ce8394c78c
 })
